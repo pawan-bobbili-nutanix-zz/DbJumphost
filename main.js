dd26361c03e9f18ce6b8a1ffb9381e2d535532b1
pawan-bobbili-nutanix <pawan.bobbili@nutanix.com>
2021-07-02 06:02:02 +0530

    ready for task

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 const anyDb = require('./anyDB');
+
+let client;
+const init = async () => {
+    try {
+        client = await anyDb();
+        return;
+    }
+    catch(err) {
+        console.log(err);
+    }
+};
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/query', async (req, res, next) => {
+    try {
+        const body = req.body;
+        const result = await client.query(body.query);
+        res.json(result);
+    }
+    catch(err) {
+        console.log(err);
+    }
+});
+
+app.listen(1345, async () => {
+    await init();
+    console.log('Started Listening on 1345');
+});
